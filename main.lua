f86cd55ba0fecf25e2dca65650fd3be7ec780d38
Keryn Knight <keryn@kerynknight.com>
2010-11-22 21:11:33 +0000

    require randomseed to get decent start numbers.
    blah, stupid Windows & OSX.

--- main.lua
+++ main.lua
@@ -2,4 +2,6 @@ local COLUMNS = 300
 local ROWS = 300
 local LEFT, UP, DOWN, RIGHT = 1, 2, 3, 4
 local EMPTY, BLACK, WHITE = -1, 0, 255
+-- cover non-randomness on Win32/OSX
+math.randomseed(os.time())
 local direction, startx, starty = math.random(4), math.random(COLUMNS), math.random(ROWS)
