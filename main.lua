d2190ef7294d82331edcaa2a2effbbc97d2bf5ce
Keryn Knight <keryn@kerynknight.com>
2010-11-22 22:15:59 +0000

    hmmm, we don't need a grid if we can grab pixels.

--- main.lua
+++ main.lua
@@ -4,17 +4,11 @@ local LEFT, UP, DOWN, RIGHT = 1, 2, 3, 4
 local EMPTY, BLACK, WHITE = -1, 0, 255
 -- cover non-randomness on Win32/OSX
 math.randomseed(os.time())
-local direction, startx, starty = math.random(4), math.random(COLUMNS), math.random(ROWS)
-local grid, increment = {}, 0
-for i=0, ROWS do
-    grid[i] = {}
-    for j=0, COLUMNS do
-        grid[i][j] = EMPTY
-    end
-end
+local direction, startx, starty, increment = math.random(4), math.random(COLUMNS), math.random(ROWS), 0
 function love.load()
     love.graphics.setCaption("Langton's Ant")
 	love.graphics.setMode(COLUMNS*2, ROWS*2, false, false, 0)
+	canvas = love.image.newImageData(COLUMNS, ROWS)
 end
 function love.update(dt)
     -- if the direction is over the max value (4/RIGHT), reset it to 1/LEFT
