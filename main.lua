9b02648c28f6d8b56f174ef789a391e7da2391d4
Keryn Knight <keryn@kerynknight.com>
2010-11-22 22:17:57 +0000

    declare canvas & a variable for future rendering.

--- main.lua
+++ main.lua
@@ -5,6 +5,8 @@ local EMPTY, BLACK, WHITE = -1, 0, 255
 -- cover non-randomness on Win32/OSX
 math.randomseed(os.time())
 local direction, startx, starty, increment = math.random(4), math.random(COLUMNS), math.random(ROWS), 0
+local canvas, rendered
+
 function love.load()
     love.graphics.setCaption("Langton's Ant")
 	love.graphics.setMode(COLUMNS*2, ROWS*2, false, false, 0)
