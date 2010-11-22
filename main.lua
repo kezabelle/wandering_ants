fd66034be03c7d7e83fc8b6eeece29ea3b4ad6cf
Keryn Knight <keryn@kerynknight.com>
2010-11-22 21:08:33 +0000

    setup some constants & random starting positions.

---
+++ main.lua
@@ -0,0 +1,5 @@
+local COLUMNS = 300
+local ROWS = 300
+local LEFT, UP, DOWN, RIGHT = 1, 2, 3, 4
+local EMPTY, BLACK, WHITE = -1, 0, 255
+local direction, startx, starty = math.random(4), math.random(COLUMNS), math.random(ROWS)
