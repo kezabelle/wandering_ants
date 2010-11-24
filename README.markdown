2dc222bc79d6a1425eaef1241387aea66262e482
Keryn Knight <keryn@kerynknight.com>
2010-11-24 22:53:55 +0000

    Added functionality to spawn N ants by pressing numbers on the keyboard.

--- README.markdown
+++ README.markdown
@@ -7,7 +7,9 @@ Tested against Windows & OSX. The OSX version seems to enjoy destroying my fan,
 so there's that.
 
 Started out doing marching left/right based on absolute black/white (as RGBA),
-but it's kind of boring, so now it does so based on greyscale values.
+but it's kind of boring, so now it does so based on greyscale values. In theory,
+pressing any number from 1-9 will reset the canvas and spawn the appropriate
+number of ants, designated on the canvas in decadent red.
 
 Unless I've gone to the effort of deleting it, the source should be available via
 [git], hosted on [github].
--- main.lua
+++ main.lua
@@ -1,75 +1,105 @@
-local COLUMNS = 50
-local ROWS = 50
-local SCALE = 10
+-- bound defaults
+local COLUMNS, ROWS, SCALE = 50, 50, 10
 local LEFT, UP, DOWN, RIGHT = 1, 2, 3, 4
 local EMPTY, BLACK, WHITE = -1, 0, 255
--- cover non-randomness on Win32/OSX
-math.randomseed(os.time())
-local direction, startx, starty, increment = math.random(4), math.random(COLUMNS), math.random(ROWS), 0
-local canvas, rendered
+local keys = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+-- unbound on startup
+local ants, direction, startx, starty, canvas, rendered
+
 
 function love.load()
     love.graphics.setCaption("Langton's Ant")
 	love.graphics.setMode(COLUMNS*SCALE, ROWS*SCALE, false, true, 0)
 	love.mouse.setVisible(false)
 	canvas = love.image.newImageData(COLUMNS, ROWS)
-	-- paint everything in the canvas black
-	canvas:mapPixel(function ()
+	reset()
+	create_ants(1)
+end
+
+function reset()
+    -- paint everything in the canvas black
+    canvas:mapPixel(function ()
 	    return 0, 0, 0, 100
     end)
+    rendered = nil
+end
+
+function create_ants(count)
+    ants = {}
+    for i=1, count do
+        -- cover non-randomness on Win32/OSX
+        math.randomseed(i+count)
+        ants[i] = {
+            direction = math.random(RIGHT),
+            x_position = math.random(COLUMNS),
+            y_position = math.random(ROWS)
+        }
+    end
+end
+
+function love.keypressed(k, uni)
+    -- did we press 1-9?
+    local numkey = tonumber(k)
+    if keys[numkey] then
+        reset()
+        create_ants(numkey)
+    end
 end
 
 function love.update(dt)
-    rendered = nil
-    local r,g,b,_ = canvas:getPixel(startx, starty)
-    local rgb = r+g+b
-    local newrgb = math.random(1, 254)
-    canvas:setPixel(startx, starty, newrgb, newrgb, newrgb, 255)
-    -- 600> is 'white', 165< is 'black', change direction!
-    if rgb >= 600 then
-        direction = direction+1
-    elseif rgb <= 165 then
-        direction = direction-1
-    end
+    for _,ant in ipairs(ants) do
+        local r,g,b,_ = canvas:getPixel(ant['x_position'], ant['y_position'])
+        local rgb = r+g+b
+        local newrgb = math.random(1, 254)
+        canvas:setPixel(ant['x_position'], ant['y_position'], newrgb, newrgb, newrgb, 255)
 
-    -- if the direction is over the max value (4/RIGHT), reset it to 1/LEFT
-    -- alternatively, if its under the min (1/LEFT), set it to 4/RIGHT
-    if direction > RIGHT then
-        direction = LEFT
-    elseif direction < LEFT then
-        direction = RIGHT
-    end
+        -- 600> is 'white', 165< is 'black', change direction!
+        if rgb >= 600 then
+            ant['direction'] = ant['direction']+1
+        elseif rgb <= 165 then
+            ant['direction'] = ant['direction']-1
+        end
 
-    -- change direction
-    if direction == LEFT then
-        starty = starty-1
-    elseif direction == RIGHT then
-        startx = startx-1
-    elseif direction == DOWN then
-        starty = starty+1
-    elseif direction == UP then
-        startx = startx+1
-    end
+        -- if the direction is over the max value (4/RIGHT), reset it to 1/LEFT
+        -- alternatively, if its under the min (1/LEFT), set it to 4/RIGHT
+        if ant['direction'] > RIGHT then
+            ant['direction'] = LEFT
+        elseif ant['direction'] < LEFT then
+            ant['direction'] = RIGHT
+        end
 
-    -- handle edges of the world
-    if startx > COLUMNS or startx < 0 then
-        startx =  math.random(COLUMNS)
-        direction = math.random(RIGHT)
+        -- change direction
+        if ant['direction'] == LEFT then
+            ant['y_position'] = ant['y_position']-1
+        elseif ant['direction'] == RIGHT then
+            ant['x_position'] = ant['x_position']-1
+        elseif ant['direction'] == DOWN then
+            ant['y_position'] = ant['y_position']+1
+        elseif ant['direction'] == UP then
+            ant['x_position'] = ant['x_position']+1
+        end
+
+        -- handle edges of the world
+        if ant['x_position'] > COLUMNS or ant['x_position'] < 0 then
+            ant['x_position'] =  math.random(COLUMNS)
+            direction = math.random(RIGHT)
+        end
+        if ant['y_position'] > ROWS or ant['y_position'] < 0 then
+            ant['y_position'] = math.random(ROWS)
+            direction = math.random(RIGHT)
+        end
     end
-    if starty > ROWS or starty < 0 then
-        starty = math.random(ROWS)
-        direction = math.random(RIGHT)
-    end
+    
     rendered = love.graphics.newImage(canvas)
     rendered:setFilter('nearest', 'nearest')
 end
 
 function love.draw()
-    increment = increment+1
     love.graphics.scale(SCALE, SCALE)
     love.graphics.setColor(WHITE, WHITE, WHITE, 255)
-    -- love.graphics.print('Iteration: '..increment, 20, 20)
     love.graphics.draw(rendered, 0, 0)
     love.graphics.setColor(WHITE, 0, 0, 255)
-    love.graphics.rectangle('fill', startx, starty, 1, 1)
+    for _,ant in ipairs(ants) do
+        love.graphics.rectangle('fill', ant['x_position'], ant['y_position'], 1, 1)
+    end
 end
